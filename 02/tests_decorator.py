bcfff98d9bc53d041bbed89ca55ded1f33954b21
EvdokimovaA <kykyter2002@gmail.com>
2023-10-09 12:39:37 +0300

    Исправлен декоратор, переделаны тесты к нему

--- 02/tests_decorator.py
+++ 02/tests_decorator.py
@@ -1,3 +1,6 @@
+import contextlib
+import io
+import sys
 import time
 import unittest
 from time import sleep
@@ -9,28 +12,52 @@ class TestsDecorator(unittest.TestCase):
         self.time_sleep = 0.1
         self.calls = 5
 
-    def test_decorator_mean_calculation(self):
+    def test_return_decorated_func(self):
+        @mean(self.calls)
+        def to_be_decorated():
+            sleep(self.time_sleep)
+            return 3
+
+        result = to_be_decorated()
+        self.assertEqual(result, 3)
+
+    def test_average_execution_time_many_calls(self):
+        def to_be_decorated():
+            sleep(self.time_sleep)
+            return 3
+
+        original_stdout = sys.stdout
+        sys.stdout = io.StringIO()
+        for i in range(10):
+            to_be_decorated()
+
+        lst = sys.stdout.getvalue().split()
+        list_results = [round(float(i), 1) for i in lst]
+        check = all(i == self.time_sleep for i in list_results)
+        self.assertTrue(check)
+        sys.stdout = original_stdout
+
+    def test_print_work_time_one_call(self):
         @mean(self.calls)
         def to_be_decorated():
             sleep(self.time_sleep)
 
-        list_results = []
-        for i in range(100):
-            list_results.append(round(to_be_decorated(), 1))
+        with contextlib.redirect_stdout(io.StringIO()) as captured_output:
+            to_be_decorated()
 
-        check = all(i == self.time_sleep for i in list_results)
-        self.assertTrue(check)
+        actual_output = round(float(captured_output.getvalue()), 1)
+        self.assertEqual(actual_output, self.time_sleep)
 
     def test_incorrect_type_k(self):
-        @mean(str(self.calls))
         def to_be_decorated():
             time.sleep(self.time_sleep)
 
-        self.assertRaises(TypeError, to_be_decorated)
+        with self.assertRaises(TypeError):
+            mean(str(self.calls))(to_be_decorated)
 
     def test_incorrect_k_value(self):
-        @mean(-self.calls)
         def to_be_decorated():
             time.sleep(self.time_sleep)
 
-        self.assertRaises(ValueError, to_be_decorated)
+        with self.assertRaises(ValueError):
+            mean(-self.calls)(to_be_decorated)
